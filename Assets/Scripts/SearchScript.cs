1d840839da194a481d99ecd93d74d8086bb86116
karthik <karthikprabhu23223@gmail.com>
2023-08-30 14:40:54 +0530

    Testing SearchBar

---
+++ Assets/Scripts/SearchScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchScript : MonoBehaviour
+{
+    public GameObject Container;
+
+    public GameObject[] Element;
+
+    public GameObject SearchBar;
+
+    public int totalElements;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world");
+
+        totalElements = Container.transform.childCount;
+
+        Debug.Log("Hello world");
+        Debug.Log(totalElements);
+
+        Element = new GameObject[totalElements];
+
+        for(int i = 0; i<totalElements; i++){
+            Element[i] = Container.transform.GetChild(i).gameObject;
+        }
+
+    }
+
+    // public void Search(){
+    //     string SearchText = SearchBar.GetComponent<TMP_InputField>().text;
+
+    //     int searchTxtlength = SearchText.length;
+
+    //     int searchedElements = 0;
+
+    //     foreach(GameObject ele in Element){
+
+    //         searchedElements += 1;
+
+    //         if(ele.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text.Length >= searchTxtlength)
+    //         {
+    //             if(SearchText.ToLower() == ele.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text.Substring(0,searchTxtlength).ToLower())
+    //             {
+    //                 ele.SetActive(true);
+    //             }
+    //             else
+    //             {
+    //                 ele.SetActive(false);
+    //             }
+    //         }
+    //     }
+    // }
+}
